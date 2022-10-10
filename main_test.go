db8e973f9d2477b8b39b17ba0e43d8531da1e574
sonichigo <kurosakiichigo.songoku@gmail.com>
2022-10-10 17:46:05 +0530

    make it bettr

--- main_test.go
+++ main_test.go
@@ -20,9 +20,8 @@ var a main.App
 
 func TestMain(m *testing.M) {
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		"postgres", "password",
+		"postgres")
 
 	ensureTableExists()
 	code := m.Run()
